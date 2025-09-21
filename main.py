5723446638b14476a29196697464700a0a3bcc15
Gustavo A. Silva <gustavoalmeidasilva41@gmail.com>
2025-09-20 21:07:43 -0300

    Adição de logging para validação e envio de mensagens no produtor Kafka

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
+import logging
 import os
+import sys
 
 from confluent_kafka import KafkaException, SerializingProducer
 from confluent_kafka.admin import AdminClient
@@ -7,47 +9,58 @@ from confluent_kafka.schema_registry import SchemaRegistryClient
 from confluent_kafka.schema_registry.avro import AvroSerializer
 from fastavro import parse_schema, validate
 from flask import Flask, request, jsonify
-
-app = Flask(__name__)
+from pythonjsonlogger import json as jsonlogger
 
 KAFKA_BROKER = os.getenv("KAFKA_BROKER", "localhost:9092")
 SCHEMA_REGISTRY = os.getenv("SCHEMA_REGISTRY", "http://localhost:8081")
 SCHEMA_PATH = os.getenv("SCHEMA_PATH", "./schemas/user.avsc")
 
-with open(SCHEMA_PATH) as f:
-    schema_dict = json.load(f)
 
-parsed_schema = parse_schema(schema_dict)
+def _config_logger():
+    logging_config = logging.getLogger("mock-producer")
+    logging_config.setLevel(logging.INFO)
 
-schema_registry_conf = {"url": SCHEMA_REGISTRY}
-schema_registry_client = SchemaRegistryClient(schema_registry_conf)
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setLevel(logging.INFO)
 
-avro_serializer = AvroSerializer(
-    schema_registry_client, json.dumps(schema_dict), to_dict=None)
+    formatter = jsonlogger.JsonFormatter(
+        "%(asctime)s %(name)s %(levelname)s %(message)s",
+        json_ensure_ascii=False)
+    stream_handler.setFormatter(formatter)
 
-producer_config = {
-    "bootstrap.servers": KAFKA_BROKER,
-    "value.serializer": avro_serializer
-}
-
-producer = SerializingProducer(producer_config)
-
-admin_client = AdminClient({"bootstrap.servers": KAFKA_BROKER})
+    logging_config.addHandler(stream_handler)
+    return logging_config
 
 
-def topic_exists(topic):
+app = Flask(__name__)
+
+logger = _config_logger()
+app.logger = logger
+
+
+def _get_avro():
+    with open(SCHEMA_PATH) as f:
+        schema_dict = json.load(f)
+        return schema_dict
+
+
+def _topic_exists(topic):
     try:
+        logger.info(msg=f"Verificando existência do tópico '{topic}'")
+        admin_client = AdminClient(conf={"bootstrap.servers": KAFKA_BROKER})
         metadata = admin_client.list_topics(timeout=5)
         return topic in metadata.topics
     except KafkaException as e:
-        print(f"Erro verificando tópico: {e}")
+        logger.error(msg=f"Erro verificando tópico: {e}")
         return False
 
 
-def validate_message(schema, message):
+def _validate_message(schema, message):
     try:
+        logger.info(msg="Validando mensagem contra o schema Avro")
         return validate(message, schema)
-    except Exception:
+    except Exception as e:
+        logger.error(msg=f"Erro validando mensagem: {e}")
         return False
 
 
@@ -57,24 +70,44 @@ def produce_message():
     topic = data.get("topic")
     message = data.get("message")
 
+    schema = _get_avro()
+    parsed_schema = parse_schema(schema)
+
     if not topic or not message:
+        logger.error(msg="Campos obrigatórios ausentes")
         return jsonify({"error": "Campos obrigatórios: topic, message"}), 400
 
-    if not topic_exists(topic):
+    if not _topic_exists(topic):
+        logger.error(msg=f"Tópico '{topic}' não existe")
         return jsonify({"erros": f"Tópico '{topic}' não existe no Kafka"}), 404
 
-    if not validate_message(parsed_schema, message):
+    if not _validate_message(parsed_schema, message):
+        logger.error(msg="Mensagem não está em conformidade com o schema Avro")
         return jsonify({"error": "Mensagem não está em conformidade com o schema Avro"}), 400
 
     try:
+        logger.info(msg=f"Enviando mensagem para o tópico '{topic}': {message}")
+
+        avro_serializer = AvroSerializer(
+            schema_registry_client=SchemaRegistryClient(
+                conf={"url": SCHEMA_REGISTRY}),
+            schema_str=json.dumps(schema),
+            to_dict=None
+        )
+
+        producer = SerializingProducer(conf={
+            "bootstrap.servers": KAFKA_BROKER,
+            "value.serializer": avro_serializer
+        })
         producer.produce(topic=topic, value=message)
         producer.flush()
-        print(f"Enviado para o tópico {topic}: {message}")
+
+        logger.info(msg=f"Mensagem enviada para o tópico '{topic}'")
         return jsonify({"status": "ok", "topic": topic, "message": message}), 200
     except Exception as e:
-        print(f"Erro ao enviar mensagem: {e}")
+        logger.error(msg=f"Erro ao enviar mensagem para o Kafka: {e}")
         return jsonify({"error": str(e)}), 500
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
